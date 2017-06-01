ac1943ef4978a2371697140acd1abba87230e422
Shreyas Gite <shreyas.gite@gmail.com>
2017-06-01 11:55:13 +0200

    first draft

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# wheelai
-Self Driving Cars are hot! Learn how to build one ;)
+## Self Driving Cars and Robotics
+
+### Overview
+This repository provides code, exercises and solutions for some interesting **Self Driving Cars and Robotics** projects.
+
+### Resources
+
+**Theory**
+- [Reinforcement Learning: An Introduction (2nd Edition)](https://webdocs.cs.ualberta.ca/~sutton/book/bookdraft2016sep.pdf)
+- [David Silver's Reinforcement Learning Course](http://www0.cs.ucl.ac.uk/staff/d.silver/web/Teaching.html)
+- [Deep Learing Book by Ian Goodfellow](http://www.deeplearningbook.org)
+
+**Tutorials/Online Courses**
+- [CS231n by Andrej Karapathy](https://www.youtube.com/playlist?list=PLkt2uSq6rBVctENoVBg1TpCC7OQi31AlC)
+- [Fast AI course](http://course.fast.ai/)
+
+**Beginner Resources**
+- [Intro to machine learning](https://medium.com/@ageitgey/machine-learning-is-fun-80ea3ec3c471)
+- [Intro to python](https://www.codecademy.com/learn/python)
+- [Intro to Github](https://www.youtube.com/watch?v=0fKg7e37bQE)
+
+### Contents
+* [SDC with GTAV](gtav)
+
+* If you have any questions or suggestions, feel free to reach out to me on Twitter: [@shreyasgite](https://twitter.com/shreyasgite)
--- gtav/utils.py
+++ gtav/utils.py
@@ -1,6 +1,65 @@
+import cv2
+import numpy as np
+import win32gui, win32ui, win32con, win32api
+import win32api as wapi
 import ctypes
 import time
 
+
+def grab_screen(region=None):
+
+    hwin = win32gui.GetDesktopWindow()
+
+    if region:
+            left,top,width,height = region
+
+    else:
+        width = win32api.GetSystemMetrics(win32con.SM_CXVIRTUALSCREEN)
+        height = win32api.GetSystemMetrics(win32con.SM_CYVIRTUALSCREEN)
+        left = win32api.GetSystemMetrics(win32con.SM_XVIRTUALSCREEN)
+        top = win32api.GetSystemMetrics(win32con.SM_YVIRTUALSCREEN)
+
+    hwindc = win32gui.GetWindowDC(hwin)
+    srcdc = win32ui.CreateDCFromHandle(hwindc)
+    memdc = srcdc.CreateCompatibleDC()
+    bmp = win32ui.CreateBitmap()
+    bmp.CreateCompatibleBitmap(srcdc, width, height)
+    memdc.SelectObject(bmp)
+    memdc.BitBlt((0, 0), (width, height), srcdc, (left, top), win32con.SRCCOPY)
+
+    signedIntsArray = bmp.GetBitmapBits(True)
+    img = np.fromstring(signedIntsArray, dtype='uint8')
+    img.shape = (height,width,4)
+
+    srcdc.DeleteDC()
+    memdc.DeleteDC()
+    win32gui.ReleaseDC(hwin, hwindc)
+    win32gui.DeleteObject(bmp.GetHandle())
+
+    return cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
+
+
+##########################################
+keyList = ["\b"]
+for char in "ABCDEFGHIJKLMNOPQRSTUVWXYZ 123456789,.'£$/\\":
+    keyList.append(char)
+
+
+def key_check():
+    keys = []
+    for key in keyList:
+        if wapi.GetAsyncKeyState(ord(key)):
+            keys.append(key)
+    return keys
+
+
+def count_down(num):
+    for i in list(range(num))[::-1]:
+        print(i+1)
+        time.sleep(1)
+
+
+#############################################
 SendInput = ctypes.windll.user32.SendInput
 
 
@@ -55,9 +114,3 @@ def ReleaseKey(hexKeyCode):
     ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008 | 0x0002, 0, ctypes.pointer(extra) )
     x = Input( ctypes.c_ulong(1), ii_ )
     ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
-
-if __name__ == '__main__':
-    PressKey(0x11)
-    time.sleep(1)
-    ReleaseKey(0x11)
-    time.sleep(1)
